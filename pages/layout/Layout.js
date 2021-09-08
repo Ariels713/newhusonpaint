d1ed92c0d2aa5d24218c0f6a3c1f654099bb7d3e
Ariel Rodriguez <arielrodriguez@ariels-mac-mini.lan>
2021-09-08 15:55:07 -0400

    initial commit

---
+++ pages/layout/Layout.js
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+  return (
+    <>
+      {/* <Navbar />
+      <main>{children}</main>
+      <Footer /> */}
+      <nav>I'm the nav</nav>
+    </>
+  );
+}
